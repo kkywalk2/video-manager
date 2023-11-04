1c68a5e83f2fb673f8bfd744efb913cd0c8ea913
kkywalk2 <kkywalk2@gmail.com>
2023-11-04 18:13:00 +0900

    fix: GroupRepositoryTest에 누락된 멤버 추가

--- src/test/kotlin/me/kkywalk2/videomanager/videos/infrastructure/GroupRepositoryTest.kt
+++ src/test/kotlin/me/kkywalk2/videomanager/videos/infrastructure/GroupRepositoryTest.kt
@@ -19,7 +19,7 @@ class GroupRepositoryTest {
     // 중형테스트 이지만 정상적으로 Group의 List가 Text로 잘변환되는지 확인하기 위함
     @Test
     fun `Series 엔티티를 통해 정상적으로 Group을 저장할 수 있다`() {
-        seriesJpaRepository.save(SeriesJpaEntity(name = "my first series", groups = listOf(GroupAttribute("default group", listOf(1, 2, 3)))))
+        seriesJpaRepository.save(SeriesJpaEntity(name = "my first series", memberId = 1, groups = listOf(GroupAttribute("default group", listOf(1, 2, 3)))))
 
         val seriesJpaEntity = seriesJpaRepository.findByName("my first series")
 
